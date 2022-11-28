f888a378c2f41964f0736bc4f9327f2d7a5a0609
JTrevena <jacobtrevena@gmail.com>
2022-11-28 15:59:50 +0000

    deploy site through cli to s3 bucket

---
+++ bin/cdk-deploy.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment"
+import { Construct } from 'constructs';
+
+export class S3BucketStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+        websiteIndexDocument: 'index.html',
+        publicReadAccess: true,
+      });
+      
+      new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+        sources: [s3deploy.Source.asset('./dist')],
+        destinationBucket: websiteBucket,
+      });
+  }
+  
+}
+
+const app = new cdk.App();
+new S3BucketStack(app, 'S3BucketStack');
+app.synth();
